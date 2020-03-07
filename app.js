832415b0996a0301bda3c7c553f59eecab922da7
Caterina Paun <caterina.paun@gmail.com>
2020-03-06 16:59:49 -0800

    Initial Commit with basic functionality

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Main', header: 'Main Page' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('index', { title: 'About', header: 'About Page' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('index', { title: 'Contact', header: 'Contact Page' });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
