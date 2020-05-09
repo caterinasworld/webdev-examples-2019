241205150bb93a45221f68686f8db280dfdb599d
Caterina Paun <caterina.paun@gmail.com>
2020-05-08 17:39:40 -0700

    JavaScript example for fetching data and updating the DOM

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+const url = 'https://anapioficeandfire.com/api/books/';
+
+let app = document.querySelector('#books');
+
+console.log('test');
+
+let fetchData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data); // array of 10 objects
+      data.forEach((item) => {
+        console.log(item);
+        let div = document.createElement('div');
+        div.innerHTML = `${item.name} - ${item.numberOfPages} pages`;
+        app.append(div);
+      });
+    })
+    .catch((error) => console.log(error))
+    .finally(() => {
+      app.removeChild(document.querySelector('#loading'));
+    });
+};
+
+fetchData(url);
