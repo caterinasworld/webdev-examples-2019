925e78d06463d6f377ad04392b884834283cfc36
Caterina Paun <caterina.paun@gmail.com>
2020-05-12 14:22:20 -0700

    Express CORS middleware example

---
+++ express/07-cors.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+// app.use(cors());
+
+app.get('/without-cors', (req, res, next) => {
+  res.write('Route without CORS.');
+  res.end();
+});
+
+app.get('/with-cors', cors(), (req, res, next) => {
+  res.write('Route with CORS');
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
