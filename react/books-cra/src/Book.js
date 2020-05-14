70211274de3ee92c588734f0be19159bc66bbcd5
Caterina Paun <caterina.paun@gmail.com>
2020-05-14 13:04:57 -0700

    React application example using hooks

---
+++ react/books-cra/src/Book.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+export default function Book(props) {
+  let list = props.data.map((item, index) => {
+    return (
+      <li key={index} className='book'>
+        {item.name}
+      </li>
+    );
+  });
+
+  return <ol>{list}</ol>;
+}
