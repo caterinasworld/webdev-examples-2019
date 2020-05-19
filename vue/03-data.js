2e3f3448b4e5e70a8521576c87f358fc906d141d
Caterina Paun <caterina.paun@gmail.com>
2020-05-19 12:18:08 -0700

    Vue.js working with data example

---
+++ vue/03-data.js
@@ -0,0 +1,13 @@
+// All examples are adjusted from the Vue.js Docs: https://vuejs.org/v2/guide/#
+let app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      'A Game of Thrones',
+      'A Clash of Kings',
+      'A Storm of Swords',
+      'A Feast for Crows',
+      'A Dance with Dragons',
+    ],
+  },
+});
