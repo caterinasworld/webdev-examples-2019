eba720f2551b80b6ffbfa1e4a8936fff0a13a12d
Caterina Paun <caterina.paun@gmail.com>
2020-05-26 16:21:12 -0700

    Update: change port

--- sockets/01-hello.js
+++ sockets/01-hello.js
@@ -1,8 +1,11 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const port = 5000;
 
-server.listen(80);
+server.listen(port, () => {
+  console.log(`Application running at "http://localhost:${port}/"`);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/01-hello.html');
