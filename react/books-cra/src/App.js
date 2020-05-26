6cb6e260acead412c885e027ec08afd0e4054bbf
Caterina Paun <caterina.paun@gmail.com>
2020-05-26 16:05:33 -0700

    Add: tests for application using Jest and Enzyme
    - App.test.js - adding tests for <App /> component
    - App.js - update <App /> with className for testing
    - Book.js - update <Book /> with className for testing
    - Book.test.js - adding tests for <Book /> component
    - enzyme.js - configuration file for Enzyme
    - package.json package-lock.json - update package.json with new configurations

--- react/books-cra/src/App.js
+++ react/books-cra/src/App.js
@@ -31,5 +31,3 @@ export default function App(props) {
     </div>
   );
 }
-
-// export default App;
--- react/books-cra/src/Book.js
+++ react/books-cra/src/Book.js
@@ -10,5 +10,5 @@ export default function Book(props) {
     );
   });
 
-  return <ol>{list}</ol>;
+  return <ol className='booklist'>{list}</ol>;
 }
