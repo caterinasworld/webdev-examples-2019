dc4bb0ff9faed7500e2b407e4ea6640404628365
Caterina Paun <caterina.paun@gmail.com>
2020-07-29 15:00:43 -0700

    Update: hello world example with more comments

--- node/01-hello-world.js
+++ node/01-hello-world.js
@@ -1,13 +1,19 @@
 const http = require('http');
+const port = 5000;
 
 // create the server object
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' }); // response headers
-  res.write('Hello World, Node!'); // write a response to the client
-  res.end(); // end the response
+  // set the response status and the response headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // write a response to the client
+  res.write('Hello World, Node!');
+
+  // end the response
+  res.end();
 });
 
 // set the server to listen on port 5000
-server.listen(5000, () => {
-  console.log(`Server running at http://localhost:5000`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
