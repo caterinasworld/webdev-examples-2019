63906345257a8577ce1d461b481442bea4db7755
Caterina Paun <caterina.paun@gmail.com>
2020-10-30 17:00:14 -0700

    Update: solution to DOM manipulation exercise

--- javascript/solutions/04-dom.js
+++ javascript/solutions/04-dom.js
@@ -1,15 +1,17 @@
 // whenever the user click the 'copy' button, copy the user input to the output area
 
 // option #1
-document.querySelector('#copy').addEventListener('click', handleClick);
+// fetch JavaScript objects representing specific elements in the DOM
+let userInput = document.querySelector('#input');
+let copy = document.querySelector('#copy')
+let output =  document.querySelector('#output');
 
+// add an event listener on the target element
+copy.addEventListener('click', handleClick);
+
+// callback function to handle event
 function handleClick(event) {
-
-    let userInput = document.querySelector('#input');
-    let output =  document.querySelector('#output');
-
-    output.innerHTML = userInput.value;
-
+    output.textContent = userInput.value;
 }
 
 // option #2
@@ -18,11 +20,11 @@ function handleClick(event) {
 //     let userInput = document.getElementById('input');
 //     let output =  document.getElementById('output');
 
-//     output.innerHTML = userInput.value;
+//     output.textContent = userInput.value;
 // };
 
 // option #3
 // document.getElementById('copy').onclick = () => {
 
-//     document.getElementById('output').innerHTML = document.getElementById('input').value;
+//     document.getElementById('output').textContent = document.getElementById('input').value;
 // };
