e419803b80506afbddc2a4ef7ce46ccc4d037918
Caterina Paun <caterina.paun@gmail.com>
2020-11-05 13:36:57 -0800

    Update: middleware example

--- express/04-middleware.js
+++ express/04-middleware.js
@@ -1,9 +1,9 @@
 const express = require('express');
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  console.log(req);
   console.log(req.url);
   next();
 });
