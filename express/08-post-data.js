03f8ecdaf48b3cef898e2686f089e050e6fff75c
Caterina Paun <caterina.paun@gmail.com>
2020-11-05 13:40:23 -0800

    Update: post data in Express example

--- express/08-post-data.js
+++ express/08-post-data.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const parser = require('body-parser');
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(
   parser.urlencoded({
