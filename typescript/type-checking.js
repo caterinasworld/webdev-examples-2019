8d6feb72ac9967f9e56788a70424cb406e866f24
Caterina Paun <caterina.paun@gmail.com>
2020-11-12 17:27:35 -0800

    Add: typescript example

---
+++ typescript/type-checking.js
@@ -0,0 +1,16 @@
+var firstname = 'SpongeBob';
+var species = 'sea sponge';
+var age = 12;
+var license = false;
+// license.indexOf('SquarePants');
+var characters;
+characters = ['SpongeBob', 'Peter', 'Sandy'];
+characters.push('Plankton');
+// characters = [1, 2, 3];
+// characters.push(true);
+function square(num) {
+    return num * num;
+}
+square(9);
+// square(firstname);
+// square(false);
