6e2ff40772b3aefb3a2505ac0738b4c572e9f759
Caterina Paun <caterina.paun@gmail.com>
2021-02-17 19:02:27 -0800

    Add: javascript exercises

---
+++ javascript/exercises/04-dom.js
@@ -0,0 +1,7 @@
+// whenever the user enters input text, copy the user input to the output area
+
+// fetch JavaScript objects representing specific elements in the DOM
+
+// add an event listener on the target element
+
+// add a callback function to handle event
